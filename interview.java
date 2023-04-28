0c1f4bc21762ac008c5e814116efbb2a3d1da0fd
Joshua Dutton <joshua.d.dutton@duke.edu>
2023-04-28 19:01:47 -0400

    final push

--- interview.java
+++ interview.java
@@ -7,37 +7,79 @@ public class interview {
 
     LinkedList<listItem> merge(LinkedList<listItem> list1, LinkedList<listItem> list2) {
 
-        listItem pointer1;
-        listItem pointer2;
+        listItem pointer1 = null;
+        listItem pointer2 = null;
 
         LinkedList<listItem> returnList = new LinkedList<listItem>();
 
-        int index1 = 0;
-        int index2 = 0;
         for (int i = 0; i < list1.size() + list2.size(); i++) {
             
             if (pointer1 == null) {
-                pointer1 = list1.poll();
+                pointer1 = list1.pollFirst();
             }
-
             if (pointer2 == null) {
-                pointer2 = list2.poll();
+                pointer2 = list2.pollFirst();
             }
-            
-            if (pointer1.compareDate(pointer2) > 0) {
+            if (pointer1 == null) {
+                returnList.add(pointer2);
+                continue;
+            }
+            if (pointer2 == null) {
                 returnList.add(pointer1);
+                continue;
             }
 
-            if (pointer2.compareDate(pointer1) > 0) {
-                returnList.add(pointer2);
+            if (pointer1.compareDate(pointer2) < 0) {
+                returnList.add(pointer1);
+                pointer1 = list1.pollFirst();
+            } else {
+                if (pointer1.getTask().compareTo(pointer2.getTask()) < 0) {
+                    returnList.add(pointer1);
+                    pointer1 = list1.pollFirst();
+                } else {
+                    returnList.add(pointer2);
+                    pointer2 = list2.pollFirst();
+                }
+                
             }
-            
+
 
         }
 
-        return null;
+        return returnList;
 
     }
+
+    LinkedList<listItem> timeRange(LinkedList<listItem> myList, int endYear, int endMonth, int endDay, int startYear, int startMonth, int startDay) {
+        
+        LinkedList<listItem> returnList = new LinkedList<listItem>();
+        
+        for (listItem myItem : myList) {
+            if ((myItem.getYear() < endYear && myItem.getYear() > startYear) ||
+                ((myItem.getYear() == endYear || myItem.getYear() == startYear) && (myItem.getMonth() < endMonth && myItem.getMonth() > startMonth)) ||
+                (myItem.getYear() == endYear || myItem.getYear() == startYear) && (myItem.getMonth() == endMonth || myItem.getMonth() == startMonth) && (myItem.getDay() < endDay && myItem.getDay() > startDay)) {
+                    returnList.add(myItem);
+                    System.out.print(myItem.getTask() + ", Due: " + myItem.getMonth() + "/" + myItem.getDay() + "/" + myItem.getYear());
+                    System.out.println();
+                }
+        }
+        
+        return returnList;
+    }
+
+    LinkedList<listItem> filter(boolean DATE, boolean PRIO, boolean ASSIGNED) {
+
+        // simple to do, just filter throught the list and print just like in timeRange() if we hit the parameters
+        return null;
+    }
+
+    LinkedList<listItem> csvRead(String path, String title) {
+
+        // If I had time to do this one I would just use a java scanner
+
+        return null;
+    }
+
 }
 
 // TODO: resort the list if we change the date
@@ -60,7 +102,7 @@ class todo {
         }
             int i = 0;
         for (listItem myItem : myTodo) { // insertion sort
-            if (toInsert.compareDate(myItem) > 0) {
+            if (toInsert.compareDate(myItem) < 0) {
                 myTodo.add(i, toInsert);
             }
             
@@ -135,6 +177,10 @@ class listItem {
         return priority;
     }
 
+    String getTask() {
+        return task;
+    }
+
     int compareDate(listItem o) {
         if (((this.getPriority() == o.getPriority()) && // if we are both prioirity or not prioirty
             ((this.getYear() < o.getYear()) || // check year first
@@ -142,13 +188,13 @@ class listItem {
             (this.getYear() == o.getYear() && this.getMonth() == o.getMonth() && this.getDay() < o.getDay()))) || // then day
             (this.getPriority() == true && o.getPriority() == false)) { // if we are prioirty and the are not, we insert
             
-            return 1;
+            return -1;
         }
         if (this.getPriority() == o.getPriority() && this.getYear() == o.getYear() && this.getMonth() == o.getMonth() && this.getDay() == o.getDay()) {
             return 0;
         }
         
-        return -1;
+        return 1;
     }
 
 }
